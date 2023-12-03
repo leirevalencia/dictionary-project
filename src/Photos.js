87a49e1ab015c5a568befff0c89b25c4c39cd0a1
leirevalencia <132055494+leirevalencia@users.noreply.github.com>
2023-12-03 22:58:35 +0100

    photos

--- src/Photos.js
+++ src/Photos.js
@@ -1,5 +1,25 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
-    return "hello from Photos";
+    console.log(props.photos);
+    if (props.photos) {
+        return (
+            <div className="photos">
+                {props.photos.map(function(photo,index) {
+                    console.log(photo);
+                    return (
+                        <div>
+                            <a href={photo.src.original} target="_blank" rel="noreferrer">
+                            <img src={photo.src.tiny} key={index} alt="#" />
+                            </a>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    } else {
+        return null;
+    }
+    
 }
