552e85ea648784ba7cdd5c8cd9a4cbeb94a73df7
leirevalencia <132055494+leirevalencia@users.noreply.github.com>
2023-12-03 18:51:18 +0100

    photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,35 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let[results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response) {
         console.log(response.data[0]);
         setResults(response.data[0]);
     }
 
+function handlePexelsResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+}
+
     function search(event) {
         event.preventDefault();
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey = "gllRX2ZGoTzszzed2X59f2hW1UaOZyKTbLp1sq0koIkDwjGfGVWOdpvl";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
+        let headers =  { Authorization : `Bearer ${pexelsApiKey}` };
+
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
 
@@ -44,6 +57,7 @@ export default function Dictionary() {
                 />
             </form>
             <Results results={results}/>
+            <Photos photos={photos} />
             </section>
             
         </div>
